1f26dfb26f1dc5ea44ab783f60ae9a5514b1c881
Omar2788 <86856903+Omar2788@users.noreply.github.com>
2022-08-20 01:49:44 +0100

    v1

---
+++ src/components/about/About.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./about.css"
+import p4 from "../../img/pdp.jpeg";
+
+const About = () => {
+  return (
+    <div className='a'>
+        <div className="a-left">
+            <div className="a-card bg"></div>
+            <div className="a-card">
+                <img src={p4} alt="" className="a-img" />
+            </div>
+        </div>
+        <div className="i-right">
+            <h1 className='a-title'>About Me</h1>
+            <p className='a-sub'>
+            Software engineer graduated from North American Private University of Sfax. 
+            </p>
+            <p className='a-desc'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, eius omnis maxime officia, exercitationem, sed possimus similique nobis accusantium dolores odit reiciendis necessitatibus rem at! In doloremque rerum nemo? Velit.
+            </p>
+
+        </div>
+    </div>
+  )
+}
+
+export default About
